d3046f9ec5ee0d86a84a7586b351a7c87190899f
MaiEden <maihayo18@gmail.com>
2024-06-10 13:24:37 +0300

    stage 5

--- unittests/renderer/ImageWriterTest.java
+++ unittests/renderer/ImageWriterTest.java
@@ -13,6 +13,14 @@ class ImageWriterTest {
             for (int j=0;j<imageWriter.getNy();j++)
                 imageWriter.writePixel(i,j,new Color(java.awt.Color.PINK));
 
+        for(int i=0; i<imageWriter.getNx();i+=50)
+            for (int j=0;j<imageWriter.getNy();j++)
+                imageWriter.writePixel(i,j,new Color(java.awt.Color.black));
+
+        for(int i=0; i<imageWriter.getNy();i+=50)
+            for (int j=0;j<imageWriter.getNx();j++)
+                imageWriter.writePixel(j,i,new Color(java.awt.Color.black));
+
         imageWriter.writeToImage();
     }
 }
